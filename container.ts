ea053ef0caa7284467d158fa627d4e704d2d3e67
Nick Williams <nbransby@gmail.com>
2016-09-20 22:23:41 +0100

    add implementation

---
+++ container.ts
@@ -0,0 +1,55 @@
+import 'reflect-metadata';
+
+const factories = new Map<Function, () => {new (): Object} | Object>();
+
+function isType(binding: {new (): Object} | Object, type: any): binding is {new (): Object} {
+    return !(binding instanceof type);
+}
+
+export interface BindTo<T, S extends T> {
+    toSingleton(binding: {new (): T} | S): void
+    toFactory(method: () => S): void
+}
+
+export function bind<T, S extends T>(type: {new (...args: any[]): T}): BindTo<T, S> {
+    if(factories.has(type)) throw new Error(`${type.name} already bound to ${factories.get(type)!().constructor.name}, call rebind(${type.name}) instead if that was your intention`)
+    return rebind(type);
+}
+
+export function rebind<T, S extends T>(type: {new (...args: any[]): T}): BindTo<T, S> {
+    return {
+        toSingleton: () => (binding: {new (): T} | S) => factories.set(type, () => binding),
+        toFactory: () => (method:() => S) => factories.set(type, method)
+    }
+}
+export function unbind<T>(type: {new (...args: any[]): T}) {
+    factories.delete(type);
+}
+
+export function clearBindings() {
+    factories.clear();
+}
+
+export function obtain<T, S extends T>(type: {new (...args: any[]): T}): S {
+    const factory = factories.get(type);
+    if(factory == undefined) throw new Error(`Cannot obtain ${type.name} because it has not been bound, call bind(${type.name}).toSingleton({})`);
+    let binding = factory();
+    if(isType(binding, type)) {
+        binding = new binding;
+        factories.set(type, () => binding);
+    } 
+    return binding as S;
+}
+
+export function inject(target: Object, propertyKey: string | symbol): void {
+    Object.defineProperty(target, propertyKey, { configurable: true, get: function(this: Object) {
+        const type = Reflect.getMetadata("design:type", target, propertyKey);
+        if(type == Object) throw new Error(`Cannot inject ${target.constructor.name}.${propertyKey} as it is an interface type not known at runtime, use declaration merging to preserve type (define an empty class of the same name)`);
+        if(!factories.has(type)) throw new Error(`Cannot inject ${target.constructor.name}.${propertyKey} because ${type.name} has not been bound, call bind(${type.name}).toSingleton({})`);
+        let binding = obtain(type); 
+        Object.defineProperty(this, propertyKey, {value: binding});
+        return binding;
+    }});
+}
+
+
---
+++ tests.ts
@@ -0,0 +1,55 @@
+import {inject, bind, rebind} from './container'
+
+interface Interface {
+}
+
+interface TestService {
+    handled: boolean;
+    test(): void
+}
+
+class TestService {
+
+}
+
+class TestServiceImplementation implements TestService {
+    handled: boolean;
+
+    constructor() {
+        this.handled = false;
+    }
+
+    test(): void {
+        this.handled = true;
+        console.log('test called on TestService')
+    }
+}
+
+class TestInjectable {
+    @inject
+    service: TestService
+
+    constructor() {
+    }
+}
+
+describe("Container", () => {
+    
+    it("calls a handler method when its subscribed event is published", () => {
+        //singleton bind: will use the instance passed in 
+        bind(TestService).toSingleton(new TestServiceImplementation);
+        //singleton lazy bind: only works with no arg constructors, if you want to delay the construction of the singleton to until its needed - will create an instance on first use and reuse for all other injections 
+        bind(TestService).toSingleton(TestServiceImplementation);
+        //factory bind: if you want each injection to have its own instance - will create an instance on first use in every injection   
+        bind(TestService).toFactory(() => new TestServiceImplementation);
+        //lazy factory bind: if you want to delay the construction of a singleton who's constructor requires args to until its needed - will create an instance on first use and reuse for all other injections     
+        bind(TestService).toFactory(() => {
+            const instance = new TestServiceImplementation;
+            rebind(TestService).toSingleton(instance);
+            return instance;
+        });
+        const instance = new TestInjectable;
+        instance.service.test();
+        expect(instance.service.handled).toBe(true);
+    })
+})
